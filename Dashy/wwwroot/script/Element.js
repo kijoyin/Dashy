0566000e9abb7c6ffa4de2b9d4e1c8796f090136
Kiran Joy <kijoyin@gmail.com>
2022-06-29 08:26:11 +1000

    Making the Bodyelement js to be more generic to handle any element

---
+++ Dashy/wwwroot/script/Element.js
@@ -0,0 +1,42 @@
+﻿var getElement = function (element) {
+    return document.getElementsByTagName(element)[0];
+}
+
+var setCssClassOfElement = function (element,cssClass) {
+    getElement(element).className = cssClass;
+}
+
+var addCssClassToElement = function (element,cssClass) {
+    var element = getElement(element);
+
+    if (!element.classList.contains(cssClass)) {
+        element.classList.add(cssClass);
+    }
+}
+
+var removeCssClassFromElement = function (element,cssClass) {
+    var element = getElement(element);
+    if (element.classList.contains(cssClass))
+        element.classList.remove(cssClass);
+}
+
+var toggleCssClassOfElement = function (element, cssClass) {
+    
+    var elementItem = getElement(element);
+    if (elementItem.classList.contains(cssClass)) {
+        removeCssClassFromElement(element,cssClass);
+    }
+    else {
+        addCssClassToElement(element,cssClass);
+    }
+}
+
+var setLanguageOfElement = function (element,language) {
+    var element = getElement(element);
+    element.lang = language;
+}
+
+var setTextDirectionOfElement = function (element,direction) {
+    var element = getElement(element);
+    element.dir = direction;
+}
